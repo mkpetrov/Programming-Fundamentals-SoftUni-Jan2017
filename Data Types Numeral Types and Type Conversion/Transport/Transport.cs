83f8c3b6c6d38a9880145e6e8bef99b46b6c6b0c
mkpetrov <mkpetrov@abv.bg>
2017-01-26 00:22:25 +0200

    Transport

---
+++ Data Types Numeral Types and Type Conversion/Transport/Transport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transport
+{
+    class Transport
+    {
+        static void Main(string[] args)
+        {
+            int persons = int.Parse(Console.ReadLine());
+            int capacity = 24;
+            var courses = (int)Math.Ceiling((double)persons / capacity);
+            Console.WriteLine(courses);
+        }
+    }
+}
