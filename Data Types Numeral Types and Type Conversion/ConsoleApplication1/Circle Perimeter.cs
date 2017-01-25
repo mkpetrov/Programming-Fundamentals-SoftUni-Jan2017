b5d76bd74fa85f482a9d258875e6fd793bf38881
mkpetrov <mkpetrov@abv.bg>
2017-01-26 00:13:34 +0200

    Real Numbers

--- Data Types Numeral Types and Type Conversion/ConsoleApplication1/Circle Perimeter.cs
+++ Data Types Numeral Types and Type Conversion/ConsoleApplication1/Circle Perimeter.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            double r = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:f12}",2*Math.PI*r);
         }
     }
 }
---
+++ Data Types Numeral Types and Type Conversion/Exact Product of Real Numbers/Exact Product of Real Numbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exact_Product_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal Sum = 1;
+            for (int i = 0; i < n; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+                Sum *= number;
+            }
+            Console.WriteLine(Sum);
+        }
+    }
+}
