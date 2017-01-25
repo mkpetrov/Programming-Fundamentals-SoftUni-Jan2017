6b23d9c4c89fa6d4c9a713aec672e8b35536fa05
mkpetrov <mkpetrov@abv.bg>
2017-01-26 00:58:52 +0200

    Special Numbers

---
+++ DataTypesText and OtherTypesVariables/Special Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= n; i++)
+            {
+                int sumOfDigits = 0;
+                int digits = i;
+                while (digits>0)
+                {
+                    sumOfDigits += digits % 10;
+                    digits = digits / 10;
+                }
+                bool special = (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11);
+                Console.WriteLine("{0} -> {1}",i,special);
+            }
+        }
+    }
+}
