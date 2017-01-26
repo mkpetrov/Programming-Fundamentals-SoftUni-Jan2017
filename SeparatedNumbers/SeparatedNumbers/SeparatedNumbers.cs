92b7c305a1f651f3e1a95c42cf6c36729884e590
mkpetrov <mkpetrov@abv.bg>
2017-01-26 14:24:04 +0200

    Practicing with numbers

---
+++ SeparatedNumbers/SeparatedNumbers/SeparatedNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeparatedNumbers
+{
+    class SeparatedNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int number = 0;
+            while (n>0)
+            {
+                number += n % 10;
+                n = n / 10;
+                Console.WriteLine(number);
+                number = 0;
+            }
+        }
+    }
+}
