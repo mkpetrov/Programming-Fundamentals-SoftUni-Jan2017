c522801c35c99e4dca7e20343d782432d8003122
mkpetrov <mkpetrov@abv.bg>
2017-02-05 00:04:16 +0200

    Last problem from Methods exercises

--- Methods - Exercises/String Encryption/String Encryption.cs
+++ Methods - Exercises/String Encryption/String Encryption.cs
@@ -10,7 +10,51 @@ namespace String_Encryption
     {
         static void Main(string[] args)
         {
+            int N = int.Parse(Console.ReadLine());
+
+            var result = string.Empty;
+            for (int i = 0; i < N; i++)
+            {
+                char word = char.Parse(Console.ReadLine());
+                
+                var encrypted=(Encrypt(word));
+
+                result += encrypted;
+            }
+            Console.WriteLine(result);
             
+            
+        }
+
+        static string Encrypt(char word)
+        {
+            var asciiNumber = (int)word;
+            string encryption = string.Empty;
+
+            while (asciiNumber>=10)
+            {
+                asciiNumber /= 10;
+            }
+            encryption += asciiNumber;
+            asciiNumber = word;
+            encryption += asciiNumber % 10;
+
+            
+            
+
+            var firstSymbol = asciiNumber;
+            while (asciiNumber>=10)
+            {
+                firstSymbol = asciiNumber / 10;
+                asciiNumber = asciiNumber / 10;
+            }
+            asciiNumber = word;
+            char lastSymbol = (char)(asciiNumber - firstSymbol);
+            var frstSymbol = (char)(asciiNumber + asciiNumber % 10);
+
+
+            string Encrpt = $"{frstSymbol}{encryption}{lastSymbol}";
+            return Encrpt;
         }
     }
 }
