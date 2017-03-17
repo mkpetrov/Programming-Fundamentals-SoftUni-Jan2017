6a9e490a3115ba98876e8b7078c516c8ef260f73
mkpetrov <mkpetrov@abv.bg>
2017-03-17 22:30:16 +0200

    More LINQ

---
+++ Lambda and LINQ/Lambada Expressions/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambada_Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputLine = Console.ReadLine();
+
+            var lambadaExpression = new Dictionary<string, Dictionary<string, string>>();
+
+            while (inputLine !="lambada")
+            {
+                if (inputLine !="dance")
+                {
+                    var input = inputLine
+                        .Split(new[] { ' ', '=', '>','.' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    var selector = input[0];
+                    var selectorObject = input[1];
+                    var property = input[2];
+                    if (!lambadaExpression.ContainsKey(selector))
+                    {
+                        lambadaExpression[selector] = new Dictionary<string, string>();
+                    }
+                    lambadaExpression[selector][selectorObject] = property;
+                }
+                else
+                {
+                    lambadaExpression = lambadaExpression
+                        .ToDictionary(selector => selector.Key, selector => selector
+                         .Value
+                         .ToDictionary(selectorObject => selectorObject.Key, selectorObject => selectorObject.Key + "." + selectorObject.Value));
+                }
+
+                inputLine = Console.ReadLine();
+            }
+            foreach (var selector in lambadaExpression)
+            {
+                foreach (var selectorObject in selector.Value)
+                {
+                    Console.WriteLine("{0} => {1}.{2}",selector.Key,selectorObject.Key,selectorObject.Value);
+                }
+            }
+        }
+    }
+}
---
+++ Lambda and LINQ/Ordered Banking System/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordered_Banking_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputLine = Console.ReadLine();
+
+            var outputData = new Dictionary<string, Dictionary<string, decimal>>();
+
+            while (inputLine != "end")
+            {
+                var input = inputLine
+                    .Split(new[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var bank = input[0];
+                var account = input[1];
+                var balance = decimal.Parse(input[2]);
+
+                if (!outputData.ContainsKey(bank))
+                {
+                    outputData[bank] = new Dictionary<string, decimal>();
+
+                }
+                if (!outputData[bank].ContainsKey(account))
+                {
+                    outputData[bank][account] = 0;
+                }
+                outputData[bank][account] += balance;
+
+                inputLine = Console.ReadLine();
+            }
+
+            outputData = outputData
+                .OrderByDescending(x => x.Value.Sum(y => y.Value))
+                .ThenByDescending(x => x.Value.Max(y => y.Value))
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var entry in outputData)
+            {
+                var bankName = entry.Key;
+                var accountInfo = entry.Value;
+                accountInfo = accountInfo.OrderByDescending(x => x.Value).ToDictionary(x=>x.Key,x=>x.Value);
+
+                foreach (var name in accountInfo)
+                {
+                    Console.WriteLine("{0} -> {1} ({2})", name.Key, name.Value, bankName);
+                }
+            }
+        }
+    }
+}
