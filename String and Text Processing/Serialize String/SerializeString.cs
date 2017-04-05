0444410be990d5556f400263cd0b1ea4d18d10db
mkpetrov <mkpetrov@abv.bg>
2017-04-05 22:34:21 +0300

    Strings Home Work

---
+++ String and Text Processing/Serialize String/SerializeString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Serialize_String
+{
+    class SerializeString
+    {
+        static void Main(string[] args)
+        {
+            var inputLine = Console.ReadLine();
+
+            var output = new Dictionary<char, List<int>>();
+
+            for (int i = 0; i < inputLine.Length; i++)
+            {
+                var currentChar = inputLine[i];
+
+                if (!output.ContainsKey(currentChar))
+                {
+                    output[currentChar] = new List<int>();
+                }
+
+                output[currentChar].Add(i);
+            }
+
+            foreach (var kvp in output)
+            {
+                Console.WriteLine("{0}:{1}",kvp.Key,string.Join("/",kvp.Value));
+            }
+        }
+    }
+}
---
+++ String and Text Processing/Stateless/Stateless.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stateless
+{
+    class Stateless
+    {
+        static void Main(string[] args)
+        {
+            var state = Console.ReadLine();
+            
+
+            while (state!= "collapse")
+            {
+                var fiction = Console.ReadLine();
+
+                while (fiction.Length>0)
+                {
+                    var check = state.IndexOf(fiction);
+                    if (check>=0)
+                    {
+                        state = state.Replace(fiction, "");
+                    }
+                    fiction = fiction.Remove(0, 1);
+                    var fictionLastIndex = fiction.Length - 1;
+                    if (fictionLastIndex>=0)
+                    {
+                        fiction = fiction.Remove(fictionLastIndex, 1);
+                    }
+                    
+                }
+                if (state.Length>0)
+                {
+                    Console.WriteLine(state);
+                }
+                else
+                {
+                    Console.WriteLine("(void)");
+                }
+                state = Console.ReadLine();
+            }
+        }
+    }
+}
