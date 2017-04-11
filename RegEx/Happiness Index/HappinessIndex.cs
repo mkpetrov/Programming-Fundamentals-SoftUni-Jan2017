93fac2be0c759d0e1b7b2e8dbe5ca99d19548d28
mkpetrov <mkpetrov@abv.bg>
2017-04-11 21:57:27 +0300

    RegEx More Home Work

---
+++ RegEx/Happiness Index/HappinessIndex.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Happiness_Index
+{
+    class HappinessIndex
+    {
+        static void Main(string[] args)
+        {
+            var inputLine = Console.ReadLine();
+
+            var happyRegex = new Regex(@":\)|:D|;\)|:\*|:]|;]|:}|;}|\(:|\*:|c:|\[:|\[;");
+            var sadRegex = new Regex(@":\(|D:|;\(|:\[|;\[|:{|;{|\):|:c|]:|];");
+
+            MatchCollection happyEmotes = happyRegex.Matches(inputLine);
+            MatchCollection sadEmotes = sadRegex.Matches(inputLine);
+
+            var happyCount = 0.0;
+            var sadCount = 0.0;
+
+            foreach (Match happy in happyEmotes)
+            {
+                happyCount++;
+            }
+            foreach (Match sad in sadEmotes)
+            {
+                sadCount++;
+            }
+
+            double happinesIndex = happyCount / sadCount;
+
+            if (happinesIndex>=2)
+            {
+                Console.WriteLine("Happiness index: {0:f2} :D",happinesIndex);
+            }
+            else if (happinesIndex>1&&happinesIndex<2)
+            {
+                Console.WriteLine("Happiness index: {0:f2} :)",happinesIndex);
+            }
+            else if (happinesIndex==1)
+            {
+                Console.WriteLine("Happiness index: {0:f2} :|",happinesIndex);
+            }
+            else if (happinesIndex<1)
+            {
+                Console.WriteLine("Happiness index: {0:f2} :(",happinesIndex);
+            }
+            Console.WriteLine("[Happy count: {0}, Sad count: {1}]", happyCount, sadCount);
+        }
+    }
+}
--- RegEx/Word Encounter/WordEncounter.cs
+++ RegEx/Word Encounter/WordEncounter.cs
@@ -2,14 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
-namespace Word_Encounter
+namespace _03.Word_Encounter
 {
-    class WordEncounter
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            var filter = Console.ReadLine();
+            var line = Console.ReadLine();
+            Regex firstRegex = new Regex(@"\w+");
+            var charFilter = filter[0];
+            var numberfilter = int.Parse(filter[1].ToString());
+            var result = new List<string>();
+            Regex secondRegex = new Regex("[A-Z].+[.!?]");
+
+            while (line != "end")
+            {
+                var first = secondRegex.Match(line).ToString();
+
+                if (first == line)
+                {
+                    MatchCollection words = firstRegex.Matches(line);
+
+                    foreach (Match word in words)
+                    {
+                        var counter = 0;
+                        foreach (var symbol in word.ToString())
+                        {
+                            if (symbol == charFilter)
+                            {
+                                counter++;
+                            }
+                        }
+                        if (counter >= numberfilter)
+                        {
+                            result.Add(word.ToString());
+                        }
+                    }
+                }
+
+                line = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", result));
         }
     }
 }
