d2f2ed6669435f9933c4eaa103f0cf5989501626
mkpetrov <mkpetrov@abv.bg>
2017-05-11 20:26:29 +0300

    Exam Preparation

--- Exam Preparation/Hornet Comm/HornetComm..cs
+++ Exam Preparation/Hornet Comm/HornetComm..cs
@@ -9,105 +9,88 @@
     {
         static void Main(string[] args)
         {
-            List<string> messages = new List<string>();
-            List<string> broadcasts = new List<string>();
-
-
-            string messagePattern = @"^(\d+) \<\-\> ([a-zA-Z0-9]+)$";
-            string broadcastPattern = @"^(\D+) \<\-\> ([a-zA-Z0-9]+)$";
-
-
-            Regex messageRegex = new Regex(messagePattern);
-            Regex broadcastRegex = new Regex(broadcastPattern);
-
-
-            string inputLine = Console.ReadLine();
-
-
-            while (inputLine != "Hornet is Green")
+            var messages = new List<string>();
+            var broadcasts = new List<string>();
+            while (true)
             {
+                var inputLine = Console.ReadLine();
 
-                if (messageRegex.IsMatch(inputLine))
+                if (inputLine== "Hornet is Green")
                 {
-
-                    Match match = messageRegex.Match(inputLine);
-
-
-                    string recipientCode = string.Join("", match.Groups[1].Value.Reverse());
-
-                    string message = match.Groups[2].Value;
-
-
-                    messages.Add(recipientCode + " -> " + message);
+                    break;
                 }
 
-                if (broadcastRegex.IsMatch(inputLine))
+                var prvtMsgPattern = new Regex(@"^(\d+) \<\-\> (\d+|\w+)$");
+                var broadcastPattern = new Regex(@"^(\D+) \<-\> (\d+|\w+)$");
+
+                var privateMessage = prvtMsgPattern.Match(inputLine);
+                var broadcast = broadcastPattern.Match(inputLine);
+
+                if (privateMessage.Success)
                 {
+                    //Private Message
+                    var recipientCode = string.Join("", privateMessage.Groups[1].Value.Reverse());
+                    var prvtMessage = privateMessage.Groups[2].Value;
 
-                    Match match = broadcastRegex.Match(inputLine);
+                    messages.Add(recipientCode + " -> " + prvtMessage);
 
-                    string message = match.Groups[1].Value;
-
-
-                    string frequency = DecryptFrequency(match.Groups[2].Value);
-
-
-
-                    broadcasts.Add(frequency + " -> " + message);
                 }
-
-
-                inputLine = Console.ReadLine();
-            }
-
-            Console.WriteLine("Broadcasts:");
-
-            if (broadcasts.Count > 0)
-            {
-                Console.WriteLine(string.Join("\n", broadcasts));
-            }
-            else
-            {
-                Console.WriteLine("None");
-            }
-
-            Console.WriteLine("Messages:");
-
-            if (messages.Count > 0)
-            {
-                Console.WriteLine(string.Join("\n", messages));
-            }
-            else
-            {
-                Console.WriteLine("None");
-            }
-
-
-
-        }
-        private static string DecryptFrequency(string encryptedFrequency)
-        {
-
-            string decryptedFrequency = "";
-
-
-            foreach (char currentChar in encryptedFrequency)
-            {
-
-                if (currentChar >= 65 && currentChar <= 90)
+                else if (broadcast.Success)
                 {
-                    decryptedFrequency += (char)(currentChar + 32);
-                }
-                else if (currentChar >= 97 && currentChar <= 122)
-                {
-                    decryptedFrequency += (char)(currentChar - 32);
+                    //Broadcast
+                    var broadcastMessage = broadcast.Groups[1].Value;
+                    var frequency = string.Empty;
+
+                    foreach (var entry in broadcast.Groups[2].Value)
+                    {
+                        if (entry>=65 && entry<=90)
+                        {
+                            frequency += char.ToLower(entry);
+                        }
+                        else if (entry>=97 && entry<=122)
+                        {
+                            frequency += char.ToUpper(entry);
+                        }
+                        else
+                        {
+                            frequency += entry;
+                        }
+                    }
+
+                    broadcasts.Add(frequency + " -> "+ broadcastMessage);
                 }
                 else
                 {
-                    decryptedFrequency += currentChar;
+                    continue;
                 }
             }
-            return decryptedFrequency;
+
+            Console.WriteLine("Broadcasts:");
+            if (broadcasts.Count==0)
+            {
+                Console.WriteLine("None");
+            }
+            else
+            {
+                foreach (var broadcast in broadcasts)
+                {
+                    Console.WriteLine(string.Join("\n", broadcast));
+                }
+            }
+            
+            Console.WriteLine("Messages:");
+            if (messages.Count==0)
+            {
+                Console.WriteLine("None");
+            }
+            else
+            {
+                foreach (var message in messages)
+                {
+                    Console.WriteLine(string.Join("\n", message));
+                }
+            }
+            
         }
     }
 }
---
+++ Lambda and LINQ/LINQ Test/LinqTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Test
+{
+    class LinqTest
+    {
+        static void Main(string[] args)
+        {
+            var testList = new List<string>();
+
+            var namesToAdd = Console.ReadLine().Split(' ').ToArray();
+
+            foreach (var name in namesToAdd)
+            {
+                testList.Add(name.Reverse().);
+            }
+        }
+    }
+}
