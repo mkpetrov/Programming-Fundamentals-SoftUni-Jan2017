afe2ebb5f7de608f496e43f89bb4a5f553b9b30b
mkpetrov <mkpetrov@abv.bg>
2017-05-24 23:50:43 +0300

    Exam Preparation

---
+++ Exam Preparation/Spyfer/Spyfer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spyfer
+{
+    public class Spyfer
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            for (int i = 0; i < inputLine.Count; i++)
+            {
+
+                var currentNumber = inputLine[i];
+                if (i==0 && currentNumber == inputLine[i + 1])
+                {
+                    inputLine.RemoveAt(i + 1);
+                    i = 0;
+                   
+                }
+                else if (i !=0 && i !=inputLine.Count-1 && currentNumber==inputLine[i-1]+inputLine[i+1])
+                {
+                    inputLine.RemoveAt(i + 1);
+                    inputLine.RemoveAt(i-1);
+                    i = 0;
+                }
+                else if (i==inputLine.Count-1 && inputLine[i-1]==currentNumber)
+                {
+                    inputLine.RemoveAt(i - 1);
+                    i = 0;
+                       
+                }
+                
+                
+            }
+
+            Console.WriteLine(string.Join(" ", inputLine));
+        }
+    }
+}
